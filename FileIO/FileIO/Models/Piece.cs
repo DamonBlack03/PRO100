bdc1e7b9f8deea51eb19710bc31aa0c35e09a1e1
Damon <damon.black.dwb@gmail.com>
2018-08-01 16:18:14 -0600

    Piece class

---
+++ FileIO/FileIO/Models/Piece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO.Models
+{
+    public abstract class Piece
+    {
+        public int Color { get; set; }
+
+        public void Move(ref Piece[][] p, char from_letter, int from_number, char letter, int number)
+        {
+            p[number][letter] = this;
+            p[from_number][from_letter] = null;
+        }
+
+        public abstract bool CanMove(ref Piece[][] p, char from_letter, int from_number, char letter, int number);
+
+        public abstract override string ToString();
+    }
+}
