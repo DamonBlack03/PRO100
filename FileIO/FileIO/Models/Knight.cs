8b01cf595050dac1faaf721c0a88397ffd77093d
deanapesek <jadepesek1999@gmail.com>
2018-08-02 15:15:02 -0600

    Revert "Knight done"
    This reverts commit da152e33a0b2eafafe10657a5bfd048a0ea8dbe8.

--- FileIO/FileIO/Models/Knight.cs
+++ FileIO/FileIO/Models/Knight.cs
@@ -8,52 +8,15 @@ namespace FileIO.Models
 {
     class Knight : Piece
     {
-
+    
         public override bool CanMove(ref Piece[][] p, char from_letter, int from_number, char letter, int number, bool light)
         {
-            bool possible = false;
-            from_letter -= 'A';
-            letter -= 'A';
-
-            if ((from_letter < 8 && from_letter > -1) && (from_number < 8 && from_number > -1) &&
-                (letter < 8 && letter > -1) && (number < 8 && number > -1))
-            {
-                if (letter == (from_letter + 1) || letter == (from_letter - 1))
-                {
-                    if (number == (from_number + 2) || number == (from_number - 2))
-                    {
-                        if (p[number][letter] == null)
-                        {
-                            possible = true;
-                        }
-                        else if (p[number][letter].Color != p[from_number][from_letter].Color)
-                        {
-                            possible = true;
-                        }
-                    }
-                }
-                else if (number == (from_number + 1) || number == (from_number - 1))
-                {
-                    if (letter == (from_letter + 2) || letter == (from_letter - 2))
-                    {
-                        if (p[number][letter] == null)
-                        {
-                            possible = true;
-                        }
-                        else if (p[number][letter].Color != p[from_number][from_letter].Color)
-                        {
-                            possible = true;
-                        }
-                    }
-                }
-            }
-
-            return possible;
+            throw new NotImplementedException();
         }
 
         public override string ToString()
         {
-            return "Knight";
+            throw new NotImplementedException();
         }
     }
 }
