42b146386cfa5faa00a8409d05deb60363acc262
Damon <damon.black.dwb@gmail.com>
2018-08-01 22:20:53 -0600

    Pawn Finished

--- FileIO/FileIO/Models/Pawn.cs
+++ FileIO/FileIO/Models/Pawn.cs
@@ -11,10 +11,66 @@ namespace FileIO.Models
         public override bool CanMove(ref Piece[][] p, char from_letter, int from_number, char letter, int number, bool light)
         {
             bool possible = false;
+            from_letter -= 'A';
+            letter -= 'A';
 
-            if(letter != (from_letter - 1))
+            if((from_letter < 8 && from_letter > -1) && (from_number < 8 && from_number > -1) &&
+                (letter < 8 && letter > -1) && (number < 8 && number > -1))
             {
-
+                if (light)
+                {
+                    if (letter == (from_letter - 1))
+                    {
+                        if (from_number == number && p[number][letter] == null)
+                        {
+                            possible = true;
+                        }
+                        else if (number == (from_number - 1) || number == (from_number + 1))
+                        {
+                            if (p[number][letter] != null)
+                            {
+                                possible = true;
+                            }
+                        }
+                    }
+                    else if (from_letter == 6)
+                    {
+                        if (letter == 4 && (from_number == number))
+                        {
+                            if (p[number][letter] == null && p[number][letter - 1] == null)
+                            {
+                                possible = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (letter == (from_letter + 1))
+                    {
+                        if (from_number == number && p[number][letter] == null)
+                        {
+                            possible = true;
+                        }
+                        else if (number == (from_number - 1) || number == (from_number + 1))
+                        {
+                            if (p[number][letter] != null)
+                            {
+                                possible = true;
+                            }
+                        }
+                    }
+                    else if (from_letter == 1)
+                    {
+                        if (letter == 3 && (from_number == number))
+                        {
+                            if (p[number][letter] == null && p[number][letter - 1] == null)
+                            {
+                                possible = true;
+                            }
+                        }
+                    }
+                }
             }
 
             return possible;
