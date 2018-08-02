b7c56c5b53c889f2640e6f9dfb5136614ec8604b
deanapesek <jadepesek1999@gmail.com>
2018-08-01 23:14:13 -0600

    I added content to the CanMove method for the Bishop.cs, Queen.cs, and the Rook.cs files.

---
+++ FileIO/FileIO/Models/Bishop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO.Models
+{
+    class Bishop : Piece
+    {
+        public override bool CanMove(ref Piece[][] p, char from_letter, int from_number, char letter, int number, bool light)
+        {
+            bool possible = false;
+            from_letter -= 'A';
+            letter -= 'A';
+
+            if ((from_letter < 8 && from_letter > -1) && (from_number < 8 && from_number > -1) &&
+                (letter < 8 && letter > -1) && (number < 8 && number > -1))
+            {
+                if (light)
+                {
+                    // check diagnally somehow. Will look into this tomorrow.
+                }
+            }
+
+                return possible;
+        }
+
+        public override string ToString()
+        {
+            return "Bishop";
+        }
+    }
+}
---
+++ FileIO/FileIO/Models/Queen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO.Models
+{
+    class Queen : Piece
+    {
+        public override bool CanMove(ref Piece[][] p, char from_letter, int from_number, char letter, int number, bool light)
+        {
+            bool possible = false;
+            from_letter -= 'A';
+            letter -= 'A';
+
+            if ((from_letter < 8 && from_letter > -1) && (from_number < 8 && from_number > -1) && 
+                (letter < 8 && letter > -1) && (number < 8 && number > -1))
+            {
+                if (light)
+                {
+                    if (from_letter == letter && from_number != number)
+                    {
+                        // Loop through the spaces between the the positions
+                    }
+                    else if (from_letter != letter && from_number == number)
+                    {
+                        // Loop through the spaces between the the positions
+                    }
+                    else if (from_letter != letter && from_number != number)
+                    {
+                        // Loop through the spaces between the the positions
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            return possible;
+        }
+
+        public override string ToString()
+        {
+            return "Queen";
+        }
+    }
+}
