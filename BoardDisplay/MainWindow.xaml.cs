8a3385d659b0e8de16c8c614026e1d020d3b44d9
Damon <damon.black.dwb@gmail.com>
2018-08-09 00:10:48 -0600

    All buttons in ui added to a 2d array, may not be needed

--- BoardDisplay/MainWindow.xaml.cs
+++ BoardDisplay/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace BoardDisplay
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button[,] BoardDisplay = new Button[8,8];
         public MainWindow()
         {
             InitializeComponent();
+            //remove first 17 items
+            UIElement[] ue = Board.Children.Cast<UIElement>().ToArray<UIElement>();
+            UIElement[] buttonsTemp = new UIElement[64];
+            Button[,] buttons = new Button[8,8];
+
+            for (int i = 0; i < ue.Length; i++)
+            {
+                if(i > 16)
+                {
+                    buttonsTemp[i - 17] = ue[i];
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for(int x = 0; x < 8; x++)
+                {
+                    buttons[i, x] = (Button)buttonsTemp[count++];
+                    //string s = buttons[i,x]
+                    //Console.Write(buttons[i,x].Name + " ");
+                }
+                //Console.WriteLine("");
+            }
         }
     }
 }
