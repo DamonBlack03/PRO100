d7fa5a31bce1f9bd38db5ac773609e2749b2bf48
damon <damon.black.dwb@gmail.com>
2019-08-14 16:46:15 -0600

    Base Piece Model Added

---
+++ BoardDisplay/Pieces/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardDisplay.Pieces
+{
+    public abstract class Piece
+    {
+        public Color PieceColor { get; set; }
+        public (int, int) Position { get; set; }
+
+        public abstract bool CanMove((int, int) position);
+    }
+}
