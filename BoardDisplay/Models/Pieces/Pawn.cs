81b06426370952e401780fe9957661039d53f56d
damon <damon.black.dwb@gmail.com>
2019-08-18 19:06:23 -0600

    Updated Pawn yet again because I am forgetful

--- BoardDisplay/Models/Pieces/Pawn.cs
+++ BoardDisplay/Models/Pieces/Pawn.cs
@@ -8,23 +8,15 @@ namespace BoardDisplay.Pieces
 
         public override bool CanMove(BoardPosition newPosition)
         {
-            //if ((position.Item1 == this.Position.Item1 + 2))
-            //{
-            //    return true;
-            //}
-            //else if ((position.Item1 == Position.Item1 + 1) &&
-            //         (position.Item2 == Position.Item2 || position.Item2 == Position.Item2 + 1 || position.Item2 == Position.Item2 - 1))
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-
             return (
-                        (newPosition.Row == Position.Row + 2 && !HasMadeFirstMove && newPosition.Column == Position.Column) ||
-                        (newPosition.Row == Position.Row + 1 && newPosition.Column == Position.Column)
+                        (this.PieceColor == PieceColor.WHITE &&
+                            (newPosition.Row == Position.Row - 2 && !HasMadeFirstMove && newPosition.Column == Position.Column) ||
+                            (newPosition.Row == Position.Row - 1 && newPosition.Column == Position.Column)
+                        ) ||
+                        (this.PieceColor == PieceColor.BLACK &&
+                            (newPosition.Row == Position.Row + 2 && !HasMadeFirstMove && newPosition.Column == Position.Column) ||
+                            (newPosition.Row == Position.Row + 1 && newPosition.Column == Position.Column)
+                        )
                    ) ? 
                    true : 
                    false;
@@ -33,8 +25,16 @@ namespace BoardDisplay.Pieces
 
         public bool CanAttack(BoardPosition newPosition)
         {
-            return (newPosition.Row == Position.Row + 1) &&
-                   (newPosition.Column == Position.Column + 1 || newPosition.Column == Position.Column - 1);
+            return (
+                        (this.PieceColor == PieceColor.WHITE &&
+                            (newPosition.Row == Position.Row - 1) &&
+                            (newPosition.Column == Position.Column + 1 || newPosition.Column == Position.Column - 1)
+                        ) ||
+                        (this.PieceColor == PieceColor.BLACK &&
+                            (newPosition.Row == Position.Row + 1) &&
+                            (newPosition.Column == Position.Column + 1 || newPosition.Column == Position.Column - 1)
+                        )
+                   );
         }
 
     }
